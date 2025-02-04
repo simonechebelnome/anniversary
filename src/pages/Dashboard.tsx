158ce05aa30f15a5cf30e65447523c97edcc7fbe
simonechebelnome <simonemennafiverr@gmail.com>
2025-02-04 16:16:45 +0100

    Readded commented line

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import BATH from "../assets/bath.jpg";
-// import TITLE from "../assets/text.png";
+import TITLE from "../assets/text.png";
 import { useNavigate } from "react-router-dom";
 import { Paths } from "../enum";
 import SHRINE from '../assets/sounds/shrine.mp3'
@@ -25,8 +25,8 @@ const Dashboard = () => {
       style={{ backgroundImage: `url(${BATH})` }}
       className="flex flex-col justify-start gap-32 bg-cover bg-center h-screen"
     >
-      <div className="p-4 bg-gray-700 flex justify-center">
-        {/* <img src={TITLE} alt="" /> */}
+      <div className="p-4 bg-gray-700 flex ju0stify-center">
+        <img src={TITLE} alt="" />
       </div>
       <div className="flex justify-center">
       <button onClick={handleNavigate} className="mt-99 w-2/3 rounded-lg btn btn-xl btn-primary">Entra</button>
